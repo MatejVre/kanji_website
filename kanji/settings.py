7751dd4ef398c409187af9d6201f8aa0bc6a38c7
Matej Vrecar <matej.vrecar.2020@uni.strath.ac.uk>
2023-02-08 17:36:43 +0000

    Added changes, fixed bugs.

--- kanji/settings.py
+++ kanji/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fd=(kd!rd&u42)ffepxqngb-t1!k$l3t+a9xmts*gkpkbnfsav
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
